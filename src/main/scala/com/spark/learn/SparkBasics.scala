792e9f0138db82b094d48e7abebbc77a22b5f92e
saratikkurthy <saratikkurthy@gmail.com>
2023-07-02 11:16:05 +0530

    new

---
+++ src/main/scala/com/spark/learn/SparkBasics.scala
@@ -0,0 +1,22 @@
+package com.spark.learn
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+object SparkBasics extends App
+{
+  System.setProperty("hadoop.home.dir", "D:\\hadoop2.6.1")
+  val spark = SparkSession.builder()
+    .master("local[1]")
+    .appName("SaratLearn")
+    .getOrCreate();
+  val df = spark.read.option("header", true)
+    .csv("D:\\SparkLearningData\\CSV\\Zipcodes.CSV")
+
+  val sqlDF=df.createOrReplaceTempView("Zipcodes")
+  df.select("country", "city", "zipcode", "state")
+    .show(5)
+  spark.sql("SELECT count(zipcode), city FROM ZIPCODES group by city")
+    .show(5)
+  /**df.printSchema()
+  df.show()**/
+}
